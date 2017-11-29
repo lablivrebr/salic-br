bdc736c7f6f5906663786b8582bfd799d55156d5
Vinícius Feitosa da Silva <viniciusfesil@gmail.com>
2017-11-29 17:17:05 -0200

    [FIX] Ajuste no carregamento das cidades. #26

--- application/modules/agente/models/DbTable/Pais.php
+++ application/modules/agente/models/DbTable/Pais.php
@@ -1,7 +1,6 @@
 <?php
 class Agente_Model_DbTable_Pais extends MinC_Db_Table_Abstract
 {
-    protected $_banco = "agentes";
     protected $_name = 'Pais';
     protected $_schema = 'agentes';
     protected $_primary = 'idPais';
--- application/modules/default/models/DAO/Cidade.php
+++ application/modules/default/models/DAO/Cidade.php
@@ -1,42 +1,35 @@
 <?php
-/**
- * Modelo Cidade
- * @since 29/03/2010
- * @version 1.0
- * @package application
- * @subpackage application.models
- * @link http://www.cultura.gov.br
- *
- * @todo colocar em um modulo e no formato com DbTable, Model e Mapper.
- */
+
 class Cidade extends MinC_Db_Table_Abstract
 {
-    protected $_name = 'municipios';
+    protected $_name = 'Municipios';
     protected $_schema = 'agentes';
+    protected $_primary = 'idMunicipioIBGE';
 
-    /**
-     * Metodo para buscar as cidades de um determinado estado
-     * @access public
-     * @param integer $idUF
-     * @return object $db->fetchAll($sql)
-     */
     public function buscar($idUF)
     {
-        $sql = " SELECT idMunicipioIBGE AS id, Descricao AS descricao ";
-        $sql.= " FROM " . $this->_schema . '.' . $this->_name;
-        $sql.= " WHERE idUFIBGE = " . $idUF . " ";
-        $sql.= " ORDER BY Descricao;";
+        $query = $this->select();
+        $query->from(
+            $this->_name,
+            [
+                'id' => 'idMunicipioIBGE',
+                'descricao' => 'Descricao'
+            ],
+            $this->getSchema($this->_schema)
+        );
+        $query->where('idUFIBGE = ?', $idUF);
+        $query->order('Descricao');
 
         try
         {
             $db = Zend_Db_Table::getDefaultAdapter();
             $db->setFetchMode(Zend_DB::FETCH_OBJ);
+            return $db->fetchAll($query);
         }
         catch (Zend_Exception_Db $e)
         {
             $this->view->message = "Erro ao buscar Cidades: " . $e->getMessage();
         }
 
-        return $db->fetchAll($sql);
     }
 }
