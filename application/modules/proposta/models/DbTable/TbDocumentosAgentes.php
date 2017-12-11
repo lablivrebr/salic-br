d824a92485c6fc8d01f3a839e70f5d75385eaea4
Pedro Philipe Alves de Oliveira <pedro.oliveira2@cultura.gov.br>
2017-12-11 17:50:04 -0200

    [UPDATE/FIX] Adição da propriedade 'upload.maxUploadFileSize' no arquivo de configuração da aplicação e Correções de chamadas que dão acesso ao banco de dados

--- application/modules/proposta/models/DbTable/TbDocumentosAgentes.php
+++ application/modules/proposta/models/DbTable/TbDocumentosAgentes.php
@@ -1,74 +1,33 @@
 <?php
 
-/**
- * Class Proposta_Model_DbTable_TbDocumentosAgentes
- *
- * @name Proposta_Model_DbTable_TbDocumentosAgentes
- * @package Modules/Agente
- * @subpackage Models/DbTable
- *
- * @author Ruy Junior Ferreira Silva <ruyjfs@gmail.com>
- * @since 28/09/2016
- *
- * @link http://salic.cultura.gov.br
- */
+
 class Proposta_Model_DbTable_TbDocumentosAgentes extends MinC_Db_Table_Abstract
 {
 
-    /**
-     * _banco
-     *
-     * @var bool
-     * @access protected
-     */
-    protected $_banco = 'sac';
-
-    /**
-     * _name
-     *
-     * @var bool
-     * @access protected
-     */
-    protected $_name = 'tbdocumentosagentes';
-
-    /**
-     * _schema
-     *
-     * @var string
-     * @access protected
-     */
+    protected $_name = 'tbDocumentosAgentes';
     protected $_schema = 'sac';
+    protected $_primary = 'idDocumentosAgentes';
 
-    /**
-     * _primary
-     *
-     * @var int
-     * @access protected
-     */
-    protected $_primary   = 'idDocumentosAgentes';
-
-
-    public function buscarDocumentos($where=array(), $order=array(), $tamanho=-1, $inicio=-1) {
+    public function buscarDocumentos($where = array(), $order = array(), $tamanho = -1, $inicio = -1)
+    {
         $slct = $this->select();
         $slct->setIntegrityCheck(false);
         $slct->from(
-            array("a"=>$this->_name),
+            array("a" => $this->_name),
             array("CodigoDocumento", new Zend_Db_Expr("1 as tpDoc"), 'idAgente as Codigo', 'Data', 'imDocumento', 'NoArquivo', 'TaArquivo', 'idDocumentosAgentes')
         );
         $slct->joinInner(
-            array("b"=>"DocumentosExigidos"), "a.CodigoDocumento = b.Codigo",
+            array("b" => "DocumentosExigidos"),
+            "a.CodigoDocumento = b.Codigo",
             array("Descricao"), "SAC"
         );
 
-        //adiciona quantos filtros foram enviados
         foreach ($where as $coluna => $valor) {
             $slct->where($coluna, $valor);
         }
 
-        //adicionando linha order ao select
         $slct->order($order);
 
-        // paginacao
         if ($tamanho > -1) {
             $tmpInicio = 0;
             if ($inicio > -1) {
@@ -80,29 +39,27 @@ class Proposta_Model_DbTable_TbDocumentosAgentes extends MinC_Db_Table_Abstract
     }
 
     //Essa consulta nao possui o dado bin�rio do arquivo. Somente os demais dados do arquivo!
-    public function buscarDadosDocumentos($where=array(), $order=array(), $tamanho=-1, $inicio=-1) {
+    public function buscarDadosDocumentos($where = array(), $order = array(), $tamanho = -1, $inicio = -1)
+    {
         $slct = $this->select();
         $slct->setIntegrityCheck(false);
         $slct->from(
-            array("a"=>$this->_name),
+            array("a" => $this->_name),
             array("CodigoDocumento", new Zend_Db_Expr("(1) as tpDoc"), 'idAgente as Codigo', 'Data', 'NoArquivo', 'TaArquivo', 'idDocumentosAgentes'),
             $this->_schema
         );
         $slct->joinInner(
-            array("b"=>"DocumentosExigidos"), "a.CodigoDocumento = b.Codigo",
+            array("b" => "DocumentosExigidos"), "a.CodigoDocumento = b.Codigo",
             array("Descricao"),
             $this->_schema
         );
 
-        //adiciona quantos filtros foram enviados
         foreach ($where as $coluna => $valor) {
             $slct->where($coluna, $valor);
         }
 
-        //adicionando linha order ao select
         $slct->order($order);
 
-        // paginacao
         if ($tamanho > -1) {
             $tmpInicio = 0;
             if ($inicio > -1) {
@@ -114,220 +71,214 @@ class Proposta_Model_DbTable_TbDocumentosAgentes extends MinC_Db_Table_Abstract
         return $this->fetchAll($slct);
     }
 
-    public function buscatodosdocumentos($idAgente,$idProjeto,$idPronac,$nome=0,$codigo=0,$order=array(), $tamanho=-1, $inicio=-1){
+    public function buscatodosdocumentos($idAgente, $idProjeto, $idPronac, $nome = 0, $codigo = 0, $order = array(), $tamanho = -1, $inicio = -1)
+    {
 
-        //seleciona todos os arquivos referente ao proponente do projeto
         $slct1 = $this->select();
         $slct1->setIntegrityCheck(false);
         $slct1->from(
-                array('d' => $this->_name),
-                array(
-                 "d.CodigoDocumento as CodigoDocumento",
-                 //'"Anexado pelo Proponente" as Classificacao',
-                 "d.idAgente as Codigo",
-                 "Data",
-                 "NoArquivo",
-                 "TaArquivo",
-                 "idDocumentosAgentes"),
-                array()
+            array('d' => $this->_name),
+            array(
+                "d.CodigoDocumento as CodigoDocumento",
+                "d.idAgente as Codigo",
+                "Data",
+                "NoArquivo",
+                "TaArquivo",
+                "idDocumentosAgentes"),
+            array()
         );
 
         $slct1->joinInner(
-                array('e' => 'DocumentosExigidos'),
-                'e.Codigo = d.CodigoDocumento',
-                array('Descricao',new Zend_Db_Expr("'1' as AgenteDoc, '' as ProjetoDoc"))
+            array('e' => 'DocumentosExigidos'),
+            'e.Codigo = d.CodigoDocumento',
+            array('Descricao', new Zend_Db_Expr("'1' as AgenteDoc, '' as ProjetoDoc"))
         );
 
-        $slct1->where("d.idAgente = ".$idAgente);
+        $slct1->where("d.idAgente = " . $idAgente);
 
 
         //seleciona todos os arquivos referente ao projeto -- Para arquivos anexados da forma antiga
         $slct2 = $this->select();
         $slct2->setIntegrityCheck(false);
         $slct2->from(
-                array('d' => 'tbDocumentosPreProjeto'),
-                array(
-                    "d.CodigoDocumento as CodigoDocumento",
-                    //"(Anexado pelo Proponente as Classificacao)",
-                    "idProjeto as Codigo",
-                    "Data",
-                    "NoArquivo",
-                    "TaArquivo",
-                    "idProjeto as idDocumentosAgentes"),
-                array()
+            array('d' => 'tbDocumentosPreProjeto'),
+            array(
+                "d.CodigoDocumento as CodigoDocumento",
+                "idProjeto as Codigo",
+                "Data",
+                "NoArquivo",
+                "TaArquivo",
+                "idProjeto as idDocumentosAgentes"),
+            array()
         );
-
 
 
         $slct2->joinInner(
-                array('e' => 'DocumentosExigidos'),
-                'e.Codigo = d.CodigoDocumento',
-                array('Descricao',new Zend_Db_Expr("'1' as AgenteDoc, '' as ProjetoDoc"))
+            array('e' => 'DocumentosExigidos'),
+            'e.Codigo = d.CodigoDocumento',
+            array('Descricao', new Zend_Db_Expr("'1' as AgenteDoc, '' as ProjetoDoc"))
         );
 
-        $slct2->where("idProjeto = ".$idProjeto);
+        $slct2->where("idProjeto = " . $idProjeto);
 
 
         //seleciona todos os arquivos anexados pelo minc ao projeto -- Para arquivos anexados da forma antiga
         $slct3 = $this->select();
         $slct3->setIntegrityCheck(false);
         $slct3->from(
-                array('d' => 'tbDocumento'),
-                array(
-                    "d.idTipoDocumento as CodigoDocumento",
-                    "d.idPronac as Codigo",
-                    "d.dtDocumento as Data",
-                    "d.NoArquivo",
-                    "d.TaArquivo",
-                    "d.idDocumento as idDocumentosAgentes"),
-                array()
+            array('d' => 'tbDocumento'),
+            array(
+                "d.idTipoDocumento as CodigoDocumento",
+                "d.idPronac as Codigo",
+                "d.dtDocumento as Data",
+                "d.NoArquivo",
+                "d.TaArquivo",
+                "d.idDocumento as idDocumentosAgentes"),
+            array()
         );
 
 
         $slct3->joinInner(
-                array('e' => 'tbTipoDocumento'),
-                'd.idTipoDocumento = e.idTipoDocumento',
-                array('e.dsTipoDocumento as Descricao', new Zend_Db_Expr("'' as AgenteDoc, '1' as ProjetoDoc"))
+            array('e' => 'tbTipoDocumento'),
+            'd.idTipoDocumento = e.idTipoDocumento',
+            array('e.dsTipoDocumento as Descricao', new Zend_Db_Expr("'' as AgenteDoc, '1' as ProjetoDoc"))
         );
 
-        $slct3->where("d.idPronac = ".$idPronac);
+        $slct3->where("d.idPronac = " . $idPronac);
 
 
         //seleciona todos os arquivos anexados pelo minc ao projeto no Justificativa de Manunten�?o
         $slct4 = $this->select();
         $slct4->setIntegrityCheck(false);
         $slct4->from(
-                array('d' => 'tbHistoricoAlteracaoDocumento'),
-                array(
-                    "d.idDocumentosExigidos as CodigoDocumento"
-                    //"'Anexados no Manunten�?o'",
-                    ),
-                array()
+            array('d' => 'tbHistoricoAlteracaoDocumento'),
+            array(
+                "d.idDocumentosExigidos as CodigoDocumento"
+                //"'Anexados no Manunten�?o'",
+            ),
+            array()
         );
 
 
         $slct4->joinInner(
-                array('e1' => 'tbHistoricoAlteracaoProjeto'),
-                'd.idHistoricoAlteracaoProjeto = e1.idHistoricoAlteracaoProjeto',
-                array('e1.idPRONAC AS Codigo')
+            array('e1' => 'tbHistoricoAlteracaoProjeto'),
+            'd.idHistoricoAlteracaoProjeto = e1.idHistoricoAlteracaoProjeto',
+            array('e1.idPRONAC AS Codigo')
         );
         $slct4->joinInner(
-                array('e2' => 'tbDocumento'),
-                'd.idDocumento = e2.idDocumento',
-                array(),
-                'bdcorporativo.scCorp'
+            array('e2' => 'tbDocumento'),
+            'd.idDocumento = e2.idDocumento',
+            array(),
+            'bdcorporativo.scCorp'
         );
         $slct4->joinInner(
-                array('a' => 'tbArquivo'),
-                'a.idArquivo = e2.idArquivo',
-                array('a.dtEnvio as Data', 'a.nmArquivo as NoArquivo', 'a.nrTamanho as TaArquivo', 'a.idArquivo  as idDocumentosAgentes'),
-                'bdcorporativo.scCorp'
+            array('a' => 'tbArquivo'),
+            'a.idArquivo = e2.idArquivo',
+            array('a.dtEnvio as Data', 'a.nmArquivo as NoArquivo', 'a.nrTamanho as TaArquivo', 'a.idArquivo  as idDocumentosAgentes'),
+            'bdcorporativo.scCorp'
         );
 
         $slct4->joinInner(
-                array('e3' => 'tbArquivoImagem'),
-                'a.idArquivo = e3.idArquivo',
-                array(''),
-                'bdcorporativo.scCorp'
+            array('e3' => 'tbArquivoImagem'),
+            'a.idArquivo = e3.idArquivo',
+            array(''),
+            'bdcorporativo.scCorp'
         );
-
-
 
 
         $slct4->joinLeft(
-                array('E' => 'tbTipoDocumento'),
-                'e2.idTipoDocumento = E.idTipoDocumento',
-                array('E.dsTipoDocumento as Descricao'),
-                'bdcorporativo.scCorp'
+            array('E' => 'tbTipoDocumento'),
+            'e2.idTipoDocumento = E.idTipoDocumento',
+            array('E.dsTipoDocumento as Descricao'),
+            'bdcorporativo.scCorp'
         );
 
         $slct4->joinLeft(
-                array('ArqAg' => 'tbDocumentoAgente'),
-                'ArqAg.idDocumento = e2.idDocumento',
-                array("ArqAg.idAgente as AgenteDoc"),
-                'bdcorporativo.scCorp'
+            array('ArqAg' => 'tbDocumentoAgente'),
+            'ArqAg.idDocumento = e2.idDocumento',
+            array("ArqAg.idAgente as AgenteDoc"),
+            'bdcorporativo.scCorp'
         );
 
         $slct4->joinLeft(
-                array('ArqPr' => 'tbDocumentoProjeto'),
-                'ArqPr.idDocumento = e2.idDocumento',
-               array("ArqPr.idPronac as ProjetoDoc"),
-                'bdcorporativo.scCorp'
+            array('ArqPr' => 'tbDocumentoProjeto'),
+            'ArqPr.idDocumento = e2.idDocumento',
+            array("ArqPr.idPronac as ProjetoDoc"),
+            'bdcorporativo.scCorp'
         );
 
-        $slct4->where("e1.idPronac = ".$idPronac);
-
+        $slct4->where("e1.idPronac = " . $idPronac);
 
 
         //seleciona todos os arquivos do projeto
         $slct5 = $this->select();
         $slct5->setIntegrityCheck(false);
         $slct5->from(
-                array('d' => 'tbDocumentoProjeto'),
-                array(
-                    "d.idTipoDocumento AS CodigoDocumento"
-                    //"'Anexados no Manunten�?o'",
-                    ),
-                'bdcorporativo.scCorp'
+            array('d' => 'tbDocumentoProjeto'),
+            array(
+                "d.idTipoDocumento AS CodigoDocumento"
+                //"'Anexados no Manunten�?o'",
+            ),
+            'bdcorporativo.scCorp'
         );
 
 
         $slct5->joinInner(
-                array('e1' => 'Projetos'),
-                'd.idPRONAC = e1.idPronac',
-                array('e1.idPRONAC AS Codigo')
+            array('e1' => 'Projetos'),
+            'd.idPRONAC = e1.idPronac',
+            array('e1.idPRONAC AS Codigo')
         );
         $slct5->joinInner(
-                array('e2' => 'tbDocumento'),
-                'd.idDocumento = e2.idDocumento',
-                array(),
-                'bdcorporativo.scCorp'
+            array('e2' => 'tbDocumento'),
+            'd.idDocumento = e2.idDocumento',
+            array(),
+            'bdcorporativo.scCorp'
         );
         $slct5->joinInner(
-                array('a' => 'tbArquivo'),
-                'a.idArquivo = e2.idArquivo',
-                array('a.dtEnvio as Data', 'a.nmArquivo as NoArquivo', 'a.nrTamanho as TaArquivo', 'a.idArquivo  as idDocumentosAgentes'),
-                'bdcorporativo.scCorp'
+            array('a' => 'tbArquivo'),
+            'a.idArquivo = e2.idArquivo',
+            array('a.dtEnvio as Data', 'a.nmArquivo as NoArquivo', 'a.nrTamanho as TaArquivo', 'a.idArquivo  as idDocumentosAgentes'),
+            'bdcorporativo.scCorp'
         );
 
         $slct5->joinInner(
-                array('e3' => 'tbArquivoImagem'),
-                'a.idArquivo = e3.idArquivo',
-                array(''),
-                'bdcorporativo.scCorp'
+            array('e3' => 'tbArquivoImagem'),
+            'a.idArquivo = e3.idArquivo',
+            array(''),
+            'bdcorporativo.scCorp'
         );
 
 
         $slct5->joinLeft(
-                array('E' => 'tbTipoDocumento'),
-                'e2.idTipoDocumento = E.idTipoDocumento',
-                array('E.dsTipoDocumento as Descricao'),
-                'bdcorporativo.scCorp'
+            array('E' => 'tbTipoDocumento'),
+            'e2.idTipoDocumento = E.idTipoDocumento',
+            array('E.dsTipoDocumento as Descricao'),
+            'bdcorporativo.scCorp'
         );
 
         $slct5->joinLeft(
-                array('ArqAg' => 'tbDocumentoAgente'),
-                'ArqAg.idDocumento = e2.idDocumento',
-                array("ArqAg.idAgente as AgenteDoc"),
-                'bdcorporativo.scCorp'
+            array('ArqAg' => 'tbDocumentoAgente'),
+            'ArqAg.idDocumento = e2.idDocumento',
+            array("ArqAg.idAgente as AgenteDoc"),
+            'bdcorporativo.scCorp'
         );
 
         $slct5->joinLeft(
-                array('ArqPr' => 'tbDocumentoProjeto'),
-                'ArqPr.idDocumento = e2.idDocumento',
-               array("ArqPr.idPronac as ProjetoDoc"),
-                'bdcorporativo.scCorp'
+            array('ArqPr' => 'tbDocumentoProjeto'),
+            'ArqPr.idDocumento = e2.idDocumento',
+            array("ArqPr.idPronac as ProjetoDoc"),
+            'bdcorporativo.scCorp'
         );
 
-        $slct5->where("d.idPronac = ".$idPronac);
+        $slct5->where("d.idPronac = " . $idPronac);
 
 
         //Une todas os Selects
         $slct = $this->select();
 
-        if(isset($idProjeto)){
+        if (isset($idProjeto)) {
             $slct->union(array($slct1, $slct2, $slct3, $slct4, $slct5));
-        }else{
+        } else {
             $slct->union(array($slct1, $slct3, $slct4, $slct5));
         }
 
@@ -335,10 +286,10 @@ class Proposta_Model_DbTable_TbDocumentosAgentes extends MinC_Db_Table_Abstract
         $slctMaster = $db->select();
         //$slctMaster->setIntegrityCheck(false);
         $slctMaster->from(
-                        array('Master'=>$slct),
-                        array('*'),
-                        null
-                     );
+            array('Master' => $slct),
+            array('*'),
+            null
+        );
 
 
         //adicionando linha order ao select
@@ -358,13 +309,14 @@ class Proposta_Model_DbTable_TbDocumentosAgentes extends MinC_Db_Table_Abstract
         return $db->fetchAll($slctMaster);
     }
 
-    public function abrir($id) {
+    public function abrir($id)
+    {
         $slct = $this->select();
         $slct->setIntegrityCheck(false);
 
         $slct->from(
-                array("a"=>$this->_name),
-                array("noarquivo", "imdocumento"),
+            array("a" => $this->_name),
+            array("noarquivo", "imdocumento"),
             $this->_schema
         );
 
@@ -378,7 +330,6 @@ class Proposta_Model_DbTable_TbDocumentosAgentes extends MinC_Db_Table_Abstract
     }
 
 
-
-    }
+}
 
 
