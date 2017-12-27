19bef17bf7b6629a60d738af7e9c1d69de6869f2
Vinícius Feitosa da Silva <vinicius.feitosa@cultura.gov.br>
2017-12-27 13:43:28 -0200

    [REFACTOR] Refatoração de método.

--- application/modules/default/models/vwUsuariosOrgaosGrupos.php
+++ application/modules/default/models/vwUsuariosOrgaosGrupos.php
@@ -1,33 +1,12 @@
 <?php
-/**
- * DAO vwUsuariosOrgaosGrupos
- * @since 13/09/2013
- * @version 1.0
- * @package application
- * @subpackage application.model
- * @copyright � 2011 - Minist�rio da Cultura - Todos os direitos reservados.
- * @link http://www.cultura.gov.br
- */
-
 class vwUsuariosOrgaosGrupos extends MinC_Db_Table_Abstract {
 
-    /* dados da tabela */
     protected $_banco  = 'TABELAS';
     protected $_schema = 'TABELAS';
     protected $_name   = 'vwUsuariosOrgaosGrupos';
     protected $_primary = 'usu_codigo';
 
-
-    /**
-     * Metodo para buscar as unidades autorizadas do usuario do sistema
-     * @access public
-     * @param @usu_codigo (c?digo do usu?rio)
-     * @param @sis_codigo (c?digo sistema)
-     * @param @gru_codigo (c?digo do grupo)
-     * @param @uog_orgao  (c?digo do ?rg?o)
-     * @return object
-     */
-    public function carregarPorAdmissibilidade()
+    public function buscarUnidadesAutorizadasDoUsuario($gru_nome = 'Admissibilidade')
     {
         $sql = $this->select();
         $sql->setIntegrityCheck(false);
@@ -50,24 +29,8 @@ class vwUsuariosOrgaosGrupos extends MinC_Db_Table_Abstract {
             ),
             $this->_schema
         );
-//        $sql->where('usu_codigo = ?', $usu_codigo);
-//        $sql->where('uog_status = ?', 1);
-//
-//        if (!empty($sis_codigo)) {
-//            $sql->where('sis_codigo = ?', $sis_codigo);
-//        }
-//        if (!empty($gru_codigo)) {
-//            $sql->where('gru_codigo = ?', $gru_codigo);
-//        }
-//        if (!empty($uog_orgao)) {
-//            $sql->where('uog_orgao = ?', $uog_orgao);
-//        }
-//        $sql->where('gru_codigo <> ?', 129);
-        $sql->where("gru_nome LIKE '%Admissibilidade%'");
-//        $sql->order('org_siglaautorizado ASC');
-//        $sql->order('gru_nome ASC');
+        $sql->where("gru_nome LIKE '%{$gru_nome}%'");
         $sql->order('usu_nome ASC');
-//        d($sql->__toString());
         return $this->fetchAll($sql);
     }
 
