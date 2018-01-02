2b93dca73551eb3d9da7b33dbcc51fdbf7165e34
Ananji Peixoto da Costa <ananji.costa@gmail.com>
2018-01-02 12:59:12 -0200

    [FIX]
    retirando linha de comando xdebug

--- application/modules/proposta/controllers/ManterpropostaincentivofiscalController.php
+++ application/modules/proposta/controllers/ManterpropostaincentivofiscalController.php
@@ -392,8 +392,6 @@ class Proposta_ManterpropostaincentivofiscalController extends Proposta_GenericC
             }
             return;
         } catch (Zend_Exception $ex) {
-            //$ex->getMessage()
-xd($ex->getMessage());
             if($idPreProjeto) {
                 parent::message("N&atilde;o foi poss&iacute;vel realizar a opera&ccedil;&atilde;o!" . $ex->getMessage(), "/proposta/manterpropostaincentivofiscal/index?idPreProjeto=" . $idPreProjeto, "ERROR");
             } else {
