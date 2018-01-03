d7b7e6cb902a9f087c6e5f1c7a69e10963ad4031
Cleber Santos <oclebersantos@gmail.com>
2018-01-03 18:45:32 -0200

    [Add] Adicionando mappers e models necessarias a planilha #31

---
+++ application/modules/proposta/models/DbTable/TbCustosVinculados.php
@@ -0,0 +1,8 @@
+<?php
+
+class Proposta_Model_DbTable_TbCustosVinculados extends MinC_Db_Table_Abstract
+{
+    protected $_schema = 'sac';
+    protected $_name = 'tbCustosVinculados';
+    protected $_primary = 'idCustosVinculados';
+}
